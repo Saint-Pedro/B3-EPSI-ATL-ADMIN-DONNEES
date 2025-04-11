de715cb56e8b5ff5c8c9b6fbf79e1696ac210331
juliencouraud <couraud.julien@gmail.com>
2025-04-11 15:59:37 +0200

    [feat] fix build production before deploy

--- app/api/movies/[idMovie]/route.ts
+++ app/api/movies/[idMovie]/route.ts
@@ -27,7 +27,7 @@ import { Db, MongoClient, ObjectId } from 'mongodb';
  *       500:
  *         description: Internal server error
  */
-export async function GET(request: Request, { params }: { params: { idMovie: string } }): Promise<NextResponse> {
+export async function GET(request: Request, { params }: { params: any }): Promise<NextResponse> {
   try {
     const client: MongoClient = await clientPromise;
     const db: Db = client.db('sample_mflix');
@@ -112,7 +112,7 @@ export async function POST(): Promise<NextResponse> {
  *       500:
  *         description: Internal server error
  */
-export async function PUT(request: Request, { params }: { params: { idMovie: string } }): Promise<NextResponse> {
+export async function PUT(request: Request, { params }: { params: any }): Promise<NextResponse> {
   try {
     const client: MongoClient = await clientPromise;
     const db: Db = client.db('sample_mflix');
@@ -168,7 +168,7 @@ export async function PUT(request: Request, { params }: { params: { idMovie: str
  *       500:
  *         description: Internal server error
  */
-export async function DELETE(request: Request, { params }: { params: { idMovie: string } }): Promise<NextResponse> {
+export async function DELETE(request: Request, { params }: { params: any }): Promise<NextResponse> {
   try {
     const client: MongoClient = await clientPromise;
     const db: Db = client.db('sample_mflix');
