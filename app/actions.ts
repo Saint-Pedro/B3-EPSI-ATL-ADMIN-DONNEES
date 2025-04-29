f3b51c2a2b4fa4a60bc2d3db18ad8d5af1beb3e2
Saint-Pedro <astin.laurens@yahoo.fr>
2025-04-29 17:35:55 +0200

    Corriger la connexion MongoDB

--- app/actions.ts
+++ app/actions.ts
@@ -1,19 +1,19 @@
-"use server";
+import clientPromise from "@/lib/mongodb";
 
-import client from "@/lib/mongodb";
-
-export async function testDatabaseConnection() {
+export async function testDB() {
   let isConnected = false;
   try {
-    const mongoClient = await client.connect();
+    const mongoClient = await clientPromise;
     // Send a ping to confirm a successful connection
     await mongoClient.db("admin").command({ ping: 1 });
     console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!",
-    ); // because this is a server action, the console.log will be outputted to your terminal not in the browser
-    return !isConnected;
-  } catch (e) {
-    console.error(e);
-    return isConnected;
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+    isConnected = true;
+  } catch (error) {
+    console.error("Error connecting to the database:", error);
+    throw new Error(`Error connecting to the database: ${error}`);
   }
+
+  return { isConnected };
 }
